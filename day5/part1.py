05540f5295017df1c773d59ba91784e68c5514bb
Gray-lab <martin.grym@gmail.com>
2023-12-05 00:19:08 -0600

    day 5 start

---
+++ day5/part1.py
@@ -0,0 +1,7 @@
+# each map is a range of values from source_start to source_start + range
+# and an offset = dest_start - source_start 
+
+# for each seed
+#   for each map
+#       if value in one of the ranges;
+#           value += offset for that range
